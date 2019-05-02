47888c73773f474d25ea58be8790b8eb281f4885
Lucas Czech <luc@s-cze.ch>
2019-05-02 15:09:07 +0200

    Fix placement generator script.

--- data/generate_placements.sh
+++ data/generate_placements.sh
@@ -5,7 +5,7 @@ mkdir -p jplace
 for s in 1000 2000 5000 10000 20000 50000 100000 200000 500000 1000000 ; do
 
 	echo "At size $s"
-	./gappa prepare random-placements --reference-tree trees/random-tree-1000.newick --pquery-count $s > /dev/null
+	./gappa prepare random-placements --reference-tree newick/random-tree-1000.newick --pquery-count $s > /dev/null
 	mv "random-placements.jplace" "jplace/random-placements-${s}.jplace"
 
 done
